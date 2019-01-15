c9b3f5528f440a19a68c65208e96b1132a576e27
mlakdawala <mudassirlakdawala@gmail.com>
2019-01-15 16:29:47 +0530

    No big deal

--- app/src/main/java/com/kalpv/t_kart/SplashScreenActivity.java
+++ app/src/main/java/com/kalpv/t_kart/SplashScreenActivity.java
@@ -1,13 +1,28 @@
 package com.kalpv.t_kart;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int DELAYMILLIS = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },DELAYMILLIS);
+
     }
 }
